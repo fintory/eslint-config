9dbd8cb9f0c69a19d46167d6c00f5a894f9c1b5c
Nick Schmidt <nick@kreativgebiet.com>
2019-12-10 13:32:52 +0100

    feat(config-base): move position of prettier extensions

--- packages/eslint-config-base/lib/index.js
+++ packages/eslint-config-base/lib/index.js
@@ -1,16 +1,15 @@
 module.exports = {
   root: true,
   extends: [
-    'prettier',
     './rules/best-practices',
     './rules/errors',
     './rules/es6',
     './rules/import',
     './rules/node',
-    './rules/prettier',
     './rules/react',
     './rules/strict',
     './rules/style',
     './rules/variables',
+    './rules/prettier',
   ],
 }
