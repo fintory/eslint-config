44440c4f8bd0cdb14587e969fb7628080b9e51da
Nick Schmidt <nick@kreativgebiet.com>
2019-12-28 14:45:19 +0100

    feat(eslint-config): disable `import/extensions` error for typescript files
    fixes #23

--- packages/eslint-config/lib/rules/import.js
+++ packages/eslint-config/lib/rules/import.js
@@ -1,5 +1,14 @@
 module.exports = {
   settings: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+        json: 'always',
+      },
+    ],
     'import/resolver': {
       node: {
         extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
